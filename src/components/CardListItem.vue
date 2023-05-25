94a096fff376eb6e4af3baeaf16fc18a4ea1500d
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-25 15:32:04 +0200

    add file

---
+++ src/components/CardListItem.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    props: {
+        title: String,
+        orTit: String,
+        orLang: String,
+        vote: Number,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <span>
+            Titolo: {{ title }}
+        </span>
+        <span>
+            Titolo Originale: {{ orTit }}
+        </span>
+        <span>
+            Lingua Originale: {{ orLang }}
+        </span>
+        <span>
+            Voto: {{ Math.floor(vote) }}
+        </span>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+    div {
+        border: 2px solid black;
+        color: #fff;
+        background-color: rgb(110, 110, 110,);
+        display: flex;
+        flex-direction: column;
+        max-width: calc(100% / 6 - 1em);
+    }
+    span {
+        border-bottom: 2px solid black;
+        &:last-child {
+            border: none;
+        }
+    }
+</style>
