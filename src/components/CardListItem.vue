aa01b3f6a55474c4c7b49ecc8e43da2fddf7131b
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-25 21:32:52 +0200

    add formatting/ add valutation star

--- src/components/CardListItem.vue
+++ src/components/CardListItem.vue
@@ -1,67 +1,72 @@
 <script>
-import LangFlag from 'vue-lang-code-flags';
+import LangFlag from "vue-lang-code-flags";
+import { BIconStar, BIconStarFill } from 'bootstrap-icons-vue'
 export default {
+  data() {
+    return {
+      linkPoster: `https://image.tmdb.org/t/p/w500/${this.poster}`,
+      starAverage: Math.floor(this.vote / 2),
+    };
+  },
+  props: {
+    title: String,
+    orTit: String,
+    orLang: String,
+    vote: Number,
+    poster: String,
+  },
+  methods: {
+    posterPlaceholder() {
+      this.linkPoster = "../../public/img/poster-placeholder.svg";
+    },
+  },
 
-    data() {
-        return {
-            linkPoster: `https://image.tmdb.org/t/p/w500/${this.poster}`,
-        }
-    },
-    props: {
-        title: String,
-        orTit: String,
-        orLang: String,
-        vote: Number,
-        poster: String,
-    },
-    methods: {
-        posterPlaceholder () {
-            this.linkPoster = '../../public/img/poster-placeholder.svg'
-        },
-    },
-
-    components: {
-        LangFlag,
-    },
-}
+  components: {
+    LangFlag,
+    BIconStar,
+    BIconStarFill,
+  },
+};
 </script>
 
 <template>
-    <div>
-        <img :src="linkPoster" alt="poster" @error="posterPlaceholder">
-        <span>
-            Titolo: {{ title }}
-        </span>
-        <span>
-            Titolo Originale: {{ orTit }}
-        </span>
-        <span>
-            Lingua Originale: {{ orLang }}
-            <lang-flag :iso="orLang" />
-        </span>
-        <span>
-            Voto: {{ Math.floor(vote) }}
-        </span>
+  <div class="card">
+    <img :src="linkPoster" alt="poster" @error="posterPlaceholder" />
+    <span> Titolo: {{ title }} </span>
+    <span> Titolo Originale: {{ orTit }} </span>
+    <span>
+      Lingua Originale: {{ orLang }}
+      <lang-flag :iso="orLang" />
+    </span>
+    <div id="divStar"> 
+        <template v-for="n in 5">
+            <!-- <i :class="['bi', starAverage > n ? 'bi-star-fill' : 'bi-star']"></i> -->
+            <BIconStarFill v-if="starAverage > n"/>
+            <BIconStar v-else/>
+        </template>    
     </div>
-    
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    div {
-        border: 2px solid black;
-        color: #fff;
-        background-color: rgb(110, 110, 110,);
-        display: flex;
-        flex-direction: column;
-        max-width: calc(100% / 6 - 1em);
-    }
-    span {
-        border-bottom: 2px solid black;
-        &:last-child {
-            border: none;
-        }
-    }
-    img {
-        max-width: 100%;
-    }
+.card {
+  border: 2px solid black;
+  color: #fff;
+  background-color: rgb(110, 110, 110);
+  display: flex;
+  flex-direction: column;
+  max-width: calc(100% / 6 - 1em);
+}
+span {
+  border-bottom: 2px solid black;
+  &:last-child {
+    border: none;
+  }
+}
+img {
+  max-width: 100%;
+}
+#divStar {
+    display: flex;
+}
 </style>
